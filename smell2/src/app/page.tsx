8bd7dedd73e737d768bb2f039b2e517edde5a163
秋田優斗 <yuto@akitayuutonoMacBook-Air.local>
2025-06-28 04:03:44 +0900

    いい感じにした

--- smell2/src/app/page.tsx
+++ smell2/src/app/page.tsx
@@ -1,10 +1,6 @@
-import { LoginButton } from '../components/LoginButtuon/LoginButton';
+// app/page.tsx
+import { redirect } from 'next/navigation';
 
-export default function Page() {
-  return (
-    <main className="p-8 text-center">
-      <h1 className="text-2xl mb-4">ようこそ！</h1>
-      <LoginButton />
-    </main>
-  );
+export default function RootRedirect() {
+  redirect('/home');
 }
