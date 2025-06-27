b6d3bb50383fb75589739681c245db4485d37b15
秋田優斗 <yuto@akitayuutonoMacBook-Air.local>
2025-06-28 00:51:34 +0900

    fix3

--- smell2/src/components/Header/Header.tsx
+++ smell2/src/components/Header/Header.tsx
@@ -11,7 +11,6 @@ import {
   MenuItem,
   IconButton,
   Collapse,
-  useTheme,
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import SearchIcon from '@mui/icons-material/Search';
